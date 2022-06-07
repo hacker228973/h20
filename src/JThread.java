a4b448eacc2cc2347cdda26b9e4ba495bc3c03a0
hacker228973 <kodiy9@gmail.com>
2022-06-07 13:20:56 +0200

    1.1

--- src/JThread.java
+++ src/JThread.java
@@ -1,9 +1,7 @@
-import com.sun.deploy.panel.JHighDPITable;
 
-import java.util.ArrayList;
 
 class JThread extends Thread {
-    static int counter = 0;
+
 
     JThread(String name) {
         super(name);
@@ -13,14 +11,10 @@ class JThread extends Thread {
 
         System.out.printf("%s started... \n", Thread.currentThread().getName());
         for (int i = 0; i < 1000; i++) {
-            counter = Program.nextCounter(counter);
+            Program.COUNTER = Program.nextCounter(Program.COUNTER);
 
         }
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            System.out.println("Thread has been interrupted");
-        }
+
         System.out.printf("%s finished... \n", Thread.currentThread().getName());
     }
 
@@ -31,20 +25,16 @@ class JThread extends Thread {
 }
 
 class Program {
+    static int COUNTER = 0;
 
     public static void main(String[] args) {
 
         Thread[] ThreadList = new Thread[10];
         System.out.println("Main thread started...");
-//        for(int i=1; i <= 10; i++)
-//            new JThread("JThread " + i).start();
-//        JThread t = new JThread("JThread 1");
-//        t.start();
-//        JThread s = new JThread("JThread 2");
-//        s.start();
+
 
         for (int i = 0; i < 10; i++) {
-//            JThread k = new JThread("JThread "+i);
+
             ThreadList[i] = new JThread("JThread " + i);
             ;
             ThreadList[i].start();
@@ -53,22 +43,15 @@ class Program {
         for (int i = 0; i < 10; i++) {
             try {
                 ThreadList[i].join();
-                if (i == 9) {
-                    System.out.println(JThread.counter);
-                }
+
             } catch (InterruptedException e) {
 
                 System.out.printf("%s has been interrupted", ThreadList[i].getName());
             }
 
         }
+        System.out.println(COUNTER);
 
-//        try {
-//            t.join();
-//        } catch (InterruptedException e) {
-//
-//            System.out.printf("%s has been interrupted", t.getName());
-//        }
 
         System.out.println("Main thread finished...");
     }
